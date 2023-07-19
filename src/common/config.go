385cbe3088e18a6805067bf86e16343dd0bdb658
Deepak Sapkota <iamdpk.spk@gmail.com>
2023-07-19 12:01:47 +0545

    feat(env): add config str and file

---
+++ src/common/config.go
@@ -0,0 +1,31 @@
+package common
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// configuration strcu that hold
+// application running port and db connection string
+type Config struct {
+	Port               string `mapstructure:"port"`
+	DbConnectionString string `mapstructure:"dbconnectionstring"`
+}
+
+// config varibale to access from other files/package
+var AppConfig *Config
+
+// load configuration data using viper
+func LoadAppConfig() {
+	log.Println("Loading Server Configuration")
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
+	if err := viper.Unmarshal(&AppConfig); err != nil {
+		log.Fatal(err)
+	}
+}
