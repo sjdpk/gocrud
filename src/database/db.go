805c6826487355c15d29f645ba88eb3d248b3b12
Deepak Sapkota <iamdpk.spk@gmail.com>
2023-07-20 11:45:27 +0545

    feat(db): implement sqlx db connect func

--- src/database/db.go
+++ src/database/db.go
@@ -3,15 +3,15 @@ package database
 import (
 	"log"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
-var Instance *gorm.DB
+var Instance *sqlx.DB
 var err error
 
 func Connect(connectionString string) {
-	Instance, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	Instance, err = sqlx.Open("postgres", connectionString)
 	if err != nil {
 		log.Fatal(err)
 		panic("cannot connect to DB")
